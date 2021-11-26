b67833268fe56280a35bcc36e720285af5fc46e1
theshivanjali <shivanjali.chaurasia@thoughts2binary.com>
2021-11-27 00:20:33 +0530

    File Formatted

--- to-do/src/components/App.js
+++ to-do/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 
 let globalId = 0;
 
@@ -11,56 +11,61 @@ function App() {
   function addTodo(event) {
     event.preventDefault();
     setTodos((oldTodos) => {
-
-		if(task !== ""){
-			setTask("");
-			return [...oldTodos, { todo: task, id: globalId++ }];
-		}else {
-			return [...oldTodos];
-		}
-
+      if (task !== "") {
+        setTask("");
+        return [...oldTodos, { todo: task, id: globalId++ }];
+      } else {
+        return [...oldTodos];
+      }
     });
   }
 
   function deleteItem(itemID) {
-	setTodos(oldTodos => oldTodos.filter(item => item.id !== itemID));
+    setTodos((oldTodos) => oldTodos.filter((item) => item.id !== itemID));
   }
 
   return (
     <div className="container">
       <header className="card">
-	  	<h1>To Do List</h1>
-	  </header>
-	  <main>
-		  <div className="card add">
-			<form onSubmit={addTodo} className="add-form">
-	<div className="txt-container">
-	<input
-		className="txt-input"
-		type="text"
-		value={task}
-		placeholder="What's on your mind today...?"
-		onChange={(event) => setTask(event.target.value)}
-	/>
-	</div>
-	<div className="btn-container">
-		<button type="submit" id="add-btn">+</button>
-				</div>
-			</form>
-		  </div>
-      <ul className="todos">
-        {todos.map((item) => {
-          return (
-            <div className="card" key={item.id}>
-			<li className="todo-item">
-				<p>{item.todo}</p>
-			</li>
-			<button className="delete-btn" onClick={()=> deleteItem(item.id)}>X</button>
+        <h1>To Do List</h1>
+      </header>
+      <main>
+        <div className="card add">
+          <form onSubmit={addTodo} className="add-form">
+            <div className="txt-container">
+              <input
+                className="txt-input"
+                type="text"
+                value={task}
+                placeholder="What's on your mind today...?"
+                onChange={(event) => setTask(event.target.value)}
+              />
             </div>
-          );
-        })}
-      </ul>
-		</main>
+            <div className="btn-container">
+              <button type="submit" id="add-btn">
+                +
+              </button>
+            </div>
+          </form>
+        </div>
+        <ul className="todos">
+          {todos.map((item) => {
+            return (
+              <div className="card" key={item.id}>
+                <li className="todo-item">
+                  <p>{item.todo}</p>
+                </li>
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  X
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
