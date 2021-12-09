41bf32236a0bbf8552896d59be083984df3b5580
Shivanjali Chaurasia <24565124+theshivanjali@users.noreply.github.com>
2021-12-09 20:26:37 +0530

    Birthday-Reminder-UI (#4)
    Co-authored-by: theshivanjali <shivanjali.chaurasia@thoughts2binary.com>

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+	const [people, setPeople] = useState(data);
+	function clearAllRecords() {
+		setPeople([]);
+	}
+	return (
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>Clear All</button>
+			</section>
+		</main>
+	);
+}
+
+export default App;
